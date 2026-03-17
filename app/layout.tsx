6df63299366a5228805794b8a3d1c38e707f834e
ankitwariyal2003 <AnkitWariyal.21041674@gehu.ac.in>
2026-03-17 16:09:12 +0530

    Initial commit from Create Next App

---
+++ app/layout.tsx
@@ -0,0 +1,23 @@
+export const metadata = {
+  title: "Go India & Medical Seva Apps",
+  description:
+    "Explore Go India for travel and Medical Seva for healthcare services. Download now and enjoy seamless services.",
+  keywords: [
+    "Go India App",
+    "Medical Seva App",
+    "Travel App India",
+    "Healthcare App India",
+  ],
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className="bg-white text-gray-800">{children}</body>
+    </html>
+  );
+}
